d796a98abe8ede4e5d8eedf8f56572ec8ecd1b7f
Pollard123a <65486998+Pollard123a@users.noreply.github.com>
2023-03-07 16:42:35 +0530

    Adding heap changes

--- src/main/java/com/company/data/Test.java
+++ src/main/java/com/company/data/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public void testHeap() {
 
-        for(int i=0;i<1000000;i++) {
+        for(int i=0;i<1000000000;i++) {
             String str = "File is found"+i;
             System.out.println("File is found");
         }
